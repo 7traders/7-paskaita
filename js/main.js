1892fc0a34018ab89069d162d23f0132e5e6e78a
7traders <tadas@7traders.eu>
2020-11-11 15:48:12 +0200

    info

---
+++ js/main.js
@@ -0,0 +1,72 @@
+// spausdiname sveikaji skaiciu
+console.log(3241422);
+// spausdiname su kableliu skaiciu
+console.log(3.14);
+// spausdiname teksta
+console.log("dsfsdf")
+console.log('sddsff')
+
+// KINTAMIEJI
+// const - default, nekinta reiksmes
+// let - gali igyti nauja reiksme, kai perrasai
+// var - nenaudotinas
+
+// KINAMUJU TIPAI
+// skaiciai (number) = 100, 100.34
+// tekstai (string) = dsfadffa 
+// logines (bolean) = true, false
+// sarasas (array) = []
+// objektai (object) = {}
+
+
+
+const pirmasisskaicius = 10
+const antrasisskaicius = 35
+const suma = pirmasisskaicius + antrasisskaicius
+console.log(suma)
+
+const suo = "Rikis"
+const maistas = "kaulas"
+// Rikis megstamas maistas yra kaulas
+const sakinys = suo + " megstamas maistas yra " + maistas + "."
+console.log(sakinys)
+
+const heroName = "Chuck"
+const heroLastName = "Norris"
+const birth = 1940
+// Chuck Norris (80)
+const age = 2020 - birth
+const sakinys2 = heroName + " " + heroLastName + " ("+ age + ")"
+console.log(sakinys2)
+
+console.clear()
+
+let pinigine = 0
+const sausis = 100
+const vasaris = 200
+const kovas = 70
+
+// 100
+// 300
+// 370
+
+pinigine = pinigine + sausis
+console.log(pinigine)
+pinigine = pinigine + vasaris
+console.log(pinigine)
+pinigine = pinigine + kovas
+console.log(pinigine)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
